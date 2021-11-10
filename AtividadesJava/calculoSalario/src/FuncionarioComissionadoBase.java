39d377dace91fe999787f2116b22d369f7c7c235
JonnathanHalisson <jonnathan.halisson@gmail.com>
2021-11-10 10:53:49 -0300

    Atualizações

--- AtividadesJava/calculoSalario/src/FuncionarioComissionadoBase.java
+++ AtividadesJava/calculoSalario/src/FuncionarioComissionadoBase.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 public class FuncionarioComissionadoBase {
     public String nome;
     public long cpf;
@@ -14,34 +13,9 @@ public class FuncionarioComissionadoBase {
         this.totalVendas = totalVendas;
         this.valorFixo = valorFixo;
     }
-
-
-
-    public double getSalario() {
-        return ((totalVendas * percentualSobVendas) / 100) + valorFixo;
-    }
-}
-=======
-public class FuncionarioComissionadoBase {
-    public String nome;
-    public long cpf;
-    public int percentualSobVendas;
-    public double totalVendas;
-    public double valorFixo;
-
-
-    public FuncionarioComissionadoBase(String nome, long cpf, int percentualSobVendas, double totalVendas, double valorFixo) {
-        this.nome = nome;
-        this.cpf = cpf;
-        this.percentualSobVendas = percentualSobVendas;
-        this.totalVendas = totalVendas;
-        this.valorFixo = valorFixo;
-    }
-
-
+    
 
     public double getSalario() {
         return ((totalVendas * percentualSobVendas) / 100) + valorFixo;
     }
 }
->>>>>>> faef3ee6483713c00752d197064434385da9a196
--- AtividadesJava/calculoSalario/src/Principal.java
+++ AtividadesJava/calculoSalario/src/Principal.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.util.Scanner;
 
 public class Principal {
@@ -15,21 +14,4 @@ public class Principal {
         }
     }
 }
-=======
-import java.util.Scanner;
 
-public class Principal {
-    public static void main(String[] args) {
-        Scanner ver = new Scanner(System.in);
-        System.out.println("Tem salário base? s/n ");
-        String base = ver.next();
-        if (base.equals("n")) {
-            FuncionarioComissionado f = new FuncionarioComissionado("jonnathan", 44400899955L, 10, 50000);
-            System.out.print(f.nome + " seu salário é de " + f.getSalario());
-        } else if (base.equals("s")) {
-            FuncionarioComissionadoBase f = new FuncionarioComissionadoBase("jonnathan", 40540540544400L, 10, 50000, 500);
-            System.out.print(f.nome + " seu salário é de " + f.getSalario());
-        }
-    }
-}
->>>>>>> faef3ee6483713c00752d197064434385da9a196
