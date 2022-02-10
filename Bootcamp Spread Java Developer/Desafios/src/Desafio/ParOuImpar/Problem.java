3268d9f466036e31e5cd8668d660a8fc64b4f0ab
JonnathanHalisson <jonnathan.halisson@gmail.com>
2022-02-10 13:33:28 -0300

    BootCamp Java

---
+++ Bootcamp Spread Java Developer/Desafios/src/Desafio/ParOuImpar/Problem.java
@@ -0,0 +1,30 @@
+package Desafio.ParOuImpar;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+/*
+* Leia um valor inteiro N. Este valor será a quantidade de valores que serão lidos em seguida. Para cada valor lido, mostre uma mensagem em inglês dizendo se este valor lido é par (EVEN), ímpar (ODD), positivo (POSITIVE) ou negativo (NEGATIVE). No caso do valor ser igual a zero (0), embora a descrição correta seja (EVEN NULL), pois por definição zero é par, seu programa deverá imprimir apenas NULL.
+* */
+
+public class Problem {
+
+    public static void main(String[] args) throws IOException {
+        Scanner leitor = new Scanner(System.in);
+        int N = leitor.nextInt();
+        for (int i = 0; i < N; i++) {
+            int X = leitor.nextInt();
+            if (X == 0){
+                System.out.println("NULL");
+            } else if (X>0 && X%2==0){
+                System.out.println("EVEN POSITIVE");
+            } else if (X>0 && X%2==1){
+                System.out.println("ODD POSITIVE");
+            } else if (X<0 && X%2==0){
+                System.out.println("EVEN NEGATIVE");
+            } else if (X<0 && X%2==-1){
+                System.out.println("ODD NEGATIVE");
+            }
+        }
+    }
+}
